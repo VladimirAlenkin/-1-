798c897d83cb89b14f939dadbd5a48051e6ec343
Vladimir <valenkin@yandex.ru>
2023-01-16 00:07:23 +0500

    задача1

---
+++ задача_1/задача1.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Задача 1: вывести квадрат числа");
+Console.WriteLine("введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = numberA*numberA;
+Console.Write("Квадрат числа равен: ");
+Console.Write(numberB);
+
