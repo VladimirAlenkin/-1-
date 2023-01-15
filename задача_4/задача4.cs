3b2fae86b055a0ee618617235889861207064068
Vladimir <valenkin@yandex.ru>
2023-01-16 01:29:53 +0500

    задача4

---
+++ задача_4/задача4.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Задача 4: найти максимальное из трех чисел");
+Console.Write("Введите первое число ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число ");
+int numberC = Convert.ToInt32(Console.ReadLine());
+if (numberA > numberB)
+{
+    if (numberA > numberC)
+    {
+        Console.Write("Максимальное число - первое: ");
+        Console.Write(numberA);
+    }
+    else
+    {
+        Console.Write("Максимальное число - третье: ");
+        Console.Write(numberC);
+    }
+}
+else
+{
+    if (numberB > numberC)
+    {
+        Console.Write("Максимальное число - второе: ");
+        Console.Write(numberB);
+    }
+    else
+    {
+        Console.Write("Максимальное число - третье: ");
+        Console.Write(numberC);
+    }
+}
