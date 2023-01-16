fd9ceefc3602afcd9d28f26782feeb7d02042da9
Vladimir <valenkin@yandex.ru>
2023-01-16 22:59:58 +0500

    задача8

---
+++ задача_8/задача8.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Задача 8: показать вторую цифру трёхзначного числа");
+Console.WriteLine("введите трёхзначное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int numberA = numberN%10;
+int numberB = (numberN - numberA)/10;
+int numberC = numberB%10;
+Console.Write("вторая цифра - ");
+Console.Write(numberC); 
