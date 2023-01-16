1e03712877f6de3b471e13ed4096263895e62c5b
Vladimir <valenkin@yandex.ru>
2023-01-16 23:18:51 +0500

    задача9

---
+++ задача9/задача9.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Задача 9: Дано число из отрезка [10, 99]. Показать наибольшую цифру числа");
+int numberN = new Random().Next(10, 100);
+int numberA = numberN%10;
+int numberB = (numberN - numberA)/10;
+if (numberA > numberB)
+{ 
+    Console.Write("Наибольшая цифра числа: ");
+    Console.WriteLine(numberN); 
+    Console.WriteLine(numberA); 
+}
+else
+{
+    Console.Write("Наибольшая цифра числа: ");
+    Console.WriteLine(numberN); 
+    Console.WriteLine(numberB); 
+}
