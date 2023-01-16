d09cab92dcf45491d8bad9303a3e926893ee95aa
Vladimir <valenkin@yandex.ru>
2023-01-16 22:50:36 +0500

    задача7

---
+++ задача_7/задача7.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задача 7: показать последнюю цифру трёхзначного числа");
+Console.WriteLine("введите трёхзначное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+if ( numberN > 99 )
+{
+     if ( 1000 > numberN )
+    {
+        int numberA = numberN%10;
+        Console.Write("последняя цифра - ");
+        Console.Write(numberA); 
+    }
+}
+else
+{
+    Console.Write("Вы ввели некорректное число");
+}
+
