085e6c01e1da1b809999e85fc307fc3b53a918a9
Vladimir <valenkin@yandex.ru>
2023-01-16 23:57:01 +0500

    задача11

---
+++ задача_11/задача11.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задача 11: выяснить, кратно ли число заданному, если нет, вывести остаток.");
+Console.WriteLine("введите число А");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+int numberC = numberA%numberB;
+if (numberC != 0)
+{
+    Console.Write("Остаток от деления: ");
+    Console.Write(numberC); 
+}
+else
+{
+    Console.Write("Числа кратны.");
+}
