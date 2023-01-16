92962aecd5fc1fce1304f8c46c0af6a870c6bbcc
Vladimir <valenkin@yandex.ru>
2023-01-16 23:33:01 +0500

    задача10

---
+++ задача_10/задача10.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Задача 10: удалить вторую цифру трёхзначного числа");
+Console.WriteLine("введите трёхзначное число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int numberA = numberN%10; // третья цифра
+int numberB = (numberN - numberN%100)/10;//первая цифра*10
+int numberC = numberB + numberA;
+Console.Write(numberC); 
