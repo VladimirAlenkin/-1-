c1336d7f32b8bf806984fed33bc504a7378c6cbb
Vladimir <valenkin@yandex.ru>
2023-01-16 22:25:30 +0500

    задача6

---
+++ задача_6/задача6.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Задача 6: показать четные числа от 1 до N");
+Console.WriteLine("введите число  N");
+int N = Convert.ToInt32(Console.ReadLine());
+int A = 0;
+if (N > 1)
+{
+    Console.WriteLine("Чётные числа: ");
+    while (A < (N -1))
+    { 
+        A = A +2;
+        Console.WriteLine(A);
+    }
+}
+else
+{
+  Console.WriteLine("Вы ввели некорректное число"); 
+}
