93652001b7cc6d946c3e120394e371f0a62c70a5
Vladimir <valenkin@yandex.ru>
2023-01-16 13:02:48 +0500

    задача5

---
+++ задача_5/задача5.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Задача 5: выяснить является ли число чётным");
+Console.WriteLine("введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+if (numberA%2 > 0)
+{
+    Console.Write("Число не является чётным ");
+}
+else
+{
+    Console.Write("Число является чётным ");
+}
